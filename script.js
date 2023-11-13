e32b760541ef66d5c4860fdd5858b9f8bb860d2f
ALAZIZUN <148176724+ALAZIZUN@users.noreply.github.com>
2023-11-13 10:05:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Geography Teacher", "Gamer", "Muslim", "Bingung ngisi apa lagi"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 0,
+    loop: true,
+});
